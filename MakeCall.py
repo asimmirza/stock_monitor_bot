81c0853de73248876ca66f8ee5997659f32ffcb8
Asim Mirza <asim.j.mirza@gmail.com>
2020-09-27 01:58:42 +0530

    Update MakeCall.py

--- MakeCall.py
+++ MakeCall.py
@@ -2,9 +2,9 @@ from twilio.rest import Client as Call
 
 class MakeCalls:
     def CallUser():
-        client = Call("ACe6debefea0793a37c388b807abd01a9f","981649a2155c5d4243ae562a01ba3316")
-        From_number = "+18316121429"
-        To_number = "+919049084492"
+        client = Call("<>","<>")
+        From_number = "<>"
+        To_number = "<>"
         # Src_path = "http://localhost:63342/hopeless_2/response.xml"
         Src_path = "http://demo.twilio.com/docs/voice.xml"
         # Src_path = "http://www.vothla.com/Twilio_Demo.xml"
